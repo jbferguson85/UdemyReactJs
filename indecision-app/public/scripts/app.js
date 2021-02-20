cc4c604696885b83b292e43abbac9a89fb72d020
jbferguson <jbferguson85@outlook.com>
2021-02-20 13:10:13 -0600

    Move counter example to its own file.

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -29,45 +29,5 @@ var template = React.createElement(
         app.options.length ? "Here are your options" : "No options"
     )
 );
-var addOne = function addOne() {
-    console.log('addOne');
-};
-
-var minusOne = function minusOne() {
-    console.log('minusOne');
-};
-
-var reset = function reset() {
-    console.log('Reset');
-};
-
-var count = 0;
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'Count: ',
-        count
-    ),
-    React.createElement(
-        'button',
-        { onClick: addOne },
-        '+1'
-    ),
-    React.createElement(
-        'button',
-        { onClick: minusOne },
-        '-1'
-    ),
-    React.createElement(
-        'button',
-        { onClick: reset },
-        'Reset'
-    )
-);
-
 var appRoot = document.getElementById('app');
-
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -17,30 +17,6 @@ const template = (
     <p>{app.options.length ? "Here are your options" : "No options" }</p>
 </div>
 );
-const addOne = () => {
-    console.log('addOne');
-}
-
-const minusOne = () => {
-    console.log('minusOne');
-}
-
-const reset = () => {
-    console.log('Reset');
-}
-
-let count = 0;
-const templateTwo = (
-    <div>
-        <h1>Count: {count}</h1>
-        <button onClick={addOne}>+1</button>
-        <button onClick={minusOne}>-1</button>
-        <button onClick={reset}>Reset</button>
-    </div>
-);
-
-
-
 const appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
 
-ReactDOM.render(templateTwo, appRoot);
